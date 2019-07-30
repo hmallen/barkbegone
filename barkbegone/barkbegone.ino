50e0c4c2342fd26d76cf7e007fbc4363295a3b5f
hmallen <allenhm@gmail.com>
2019-07-30 18:43:32 -0500

    Added main sketch.

---
+++ barkbegone/barkbegone.ino
@@ -0,0 +1,16 @@
+#include "EspMQTTClient.h"
+#include "credentials.h"
+
+#define VIBE_PIN 2
+#define VIBE_DURATION_DEFAULT 3
+#define MQTT_TOPIC_SUB "home/remote/barkbegone"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
